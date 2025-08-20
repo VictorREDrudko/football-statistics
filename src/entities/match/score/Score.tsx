8510b4869377750d1d92c1cd63ed52df6cd84cd9
Victor RED <viktor_RED_rudko@mail.ru>
2025-08-20 13:01:20 +0300

    refactor: change score Match

--- src/entities/match/score/Score.tsx
+++ src/entities/match/score/Score.tsx
@@ -1,13 +1,13 @@
-import { MatchScore } from '@/shared/model/tournament-config/class-match/types';
+import { MatchScore } from '@/shared/model/tournament-config/class-match/types'
 import s from './Score.module.scss'
-import { convertMatchScore } from '@/shared/lib/helpers/convertMatchScore';
+import { convertMatchScore } from '@/shared/lib/helpers/convertMatchScore'
 
 export type Props = {
   score: Array<number[]>
 }
 
-export const Score = ({score}: Props) => {
-  const matchScore: MatchScore = convertMatchScore(score);
+export const Score = ({ score }: Props) => {
+  const matchScore: MatchScore = convertMatchScore(score)
 
   return (
     <div className={s.container}>
--- src/widgets/national-championship/lib/sortGroupNames.ts
+++ src/widgets/national-championship/lib/sortGroupNames.ts
@@ -1,9 +1,7 @@
-import { Match } from '@/entities/match/model/types'
+import { MatchInfo } from '@/shared/model'
 
-export const sortGroupNames = (matches: Match[]) => {
-  // if (matches[0].stage.split(":")[0].trim() === "group Final round") {
-  //   return Array.from(new Set(matches.map((match) => match.stage.split(":")[0].trim()))).sort();
-  // }
+export const sortGroupNames = (matches: MatchInfo[]) => {
+  const uniqueGroupName = new Set(matches.map((match) => match.stage))
 
-  return Array.from(new Set(matches.map((match) => match.stage))).sort()
+  return Array.from(uniqueGroupName).sort()
 }
